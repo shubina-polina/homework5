ca850531cd43987c7b389c11d28540b8494234f5
Fane Keat <02032002t@gamil.com>
2024-09-02 12:51:16 +0300

    correction

--- src/Main.java
+++ src/Main.java
@@ -38,9 +38,11 @@ public class Main {
         int deliveryDistance = 95;
         if (deliveryDistance <= 20) {
             System.out.println("Потребуется дней: 1");
-        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+        }
+        if (deliveryDistance > 20 && deliveryDistance < 60) {
             System.out.println("Потребуется дней: 2");
-        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+        }
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
             System.out.println("Потребуется дней: 3");
         } else if (deliveryDistance > 100) {
             System.out.println("Доставки нет");
@@ -92,7 +94,6 @@ public class Main {
         }
     }
 }
-
 
 
 
